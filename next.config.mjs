4fdab3e458dd06d7406d69f8abdca0cad14ea101
Isaias <isaias2011amaral@gmail.com>
2024-10-26 03:20:08 -0300

    Primeiro commit

---
+++ next.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+import path from "path";
+
+//https://imgcdn.stablediffusionweb.com/2024/4/17/3b3ceb83-440b-4402-8461-00514a71c584.jpg
+const nextConfig = {
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: "/static",
+  },
+  images: {
+    domains: ["imgcdn.stablediffusionweb.com"],
+  },
+  webpack(config, options) {
+    // Further custom configuration here
+    config.resolve.alias["Static"] = path.join(process.cwd(), "public");
+    config.resolve.alias["Public"] = path.join(process.cwd(), "public");
+
+    return config;
+  },
+};
+
+export default nextConfig;
